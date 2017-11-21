4a6b201431e6314b27ef87eacbc66eaf8a013c4c
IsaacChen <tingyi364@gmail.com>
2017-11-21 09:16:30 +0800

    santoni: Move firmware image to install/firmware-update
      * Fix derp :3
    
    Signed-off-by: IsaacChen <tingyi364@gmail.com>

--- product/releasetools.mk
+++ product/releasetools.mk
@@ -3,15 +3,15 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
 
 # Ship Firmware
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/firmware-update/adspso.bin:firmware-update/adspso.bin \
-    $(LOCAL_PATH)/firmware-update/cmnlib64.mbn:firmware-update/cmnlib64.mbn \
-    $(LOCAL_PATH)/firmware-update/cmnlib.mbn:firmware-update/cmnlib.mbn \
-    $(LOCAL_PATH)/firmware-update/devcfg.mbn:firmware-update/devcfg.mbn \
-    $(LOCAL_PATH)/firmware-update/emmc_appsboot.mbn:firmware-update/emmc_appsboot.mbn \
-    $(LOCAL_PATH)/firmware-update/keymaster.mbn:firmware-update/keymaster.mbn \
-    $(LOCAL_PATH)/firmware-update/lksecapp.mbn:firmware-update/lksecapp.mbn \
-    $(LOCAL_PATH)/firmware-update/mdtp.img:firmware-update/mdtp.img \
-    $(LOCAL_PATH)/firmware-update/NON-HLOS.bin:firmware-update/NON-HLOS.bin \
-    $(LOCAL_PATH)/firmware-update/rpm.mbn:firmware-update/rpm.mbn \
-    $(LOCAL_PATH)/firmware-update/sbl1.mbn:firmware-update/sbl1.mbn \
-    $(LOCAL_PATH)/firmware-update/tz.mbn:firmware-update/tz.mbn \
+    $(LOCAL_PATH)/firmware/adspso.bin:install/firmware-update/adspso.bin \
+    $(LOCAL_PATH)/firmware/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    $(LOCAL_PATH)/firmware/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    $(LOCAL_PATH)/firmware/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    $(LOCAL_PATH)/firmware/emmc_appsboot.mbn:install/firmware-update/emmc_appsboot.mbn \
+    $(LOCAL_PATH)/firmware/keymaster.mbn:install/firmware-update/keymaster.mbn \
+    $(LOCAL_PATH)/firmware/lksecapp.mbn:install/firmware-update/lksecapp.mbn \
+    $(LOCAL_PATH)/firmware/mdtp.img:install/firmware-update/mdtp.img \
+    $(LOCAL_PATH)/firmware/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    $(LOCAL_PATH)/firmware/rpm.mbn:install/firmware-update/rpm.mbn \
+    $(LOCAL_PATH)/firmware/sbl1.mbn:install/firmware-update/sbl1.mbn \
+    $(LOCAL_PATH)/firmware/tz.mbn:install/firmware-update/tz.mbn
