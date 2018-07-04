OnePlusOSS/android_hardware_qcom_camera

QCamera2: Change socket creation path as part of treble rules
Issue: In Android O,camera_data_file is of type core_data_file_type
       so qcamera dameon cannot have permission to access this file
       as part of treble rules.

Fix:   Created a file called vendor_camera_data_file of type data_file_type
       at /data/vendor/camera and in mm-camera-interface changing the
       socket creation path from /data/misc/camera to /data/vendor/camera.

Change-Id: I6712cadb90ec9c2d8bdf021f7e7b908c4c58f36f

QCamera2/stack/mm-camera-interface/src/mm_camera_sock.c
@@ -74,7 +74,7 @@ int mm_camera_socket_create(int cam_id, mm_camera_sock_type_t sock_type)
     memset(&sock_addr, 0, sizeof(sock_addr));
     sock_addr.addr_un.sun_family = AF_UNIX;
     snprintf(sock_addr.addr_un.sun_path,
-             UNIX_PATH_MAX, QCAMERA_DUMP_FRM_LOCATION"cam_socket%d", cam_id);
+             UNIX_PATH_MAX, "/data/vendor/camera/cam_socket%d", cam_id);
     rc = connect(socket_fd, &sock_addr.addr, sizeof(sock_addr.addr_un));
     if (0 != rc) {
       close(socket_fd);

MoKee/android_device_xiaomi_santoni

       at /data/vendor/qcam and in mm-camera-interface changing the
       socket creation path from /data/misc/camera to /data/vendor/qcam.

camera/QCamera2/stack/mm-camera-interface/src/mm_camera_sock.c
@@ -74,7 +74,7 @@ int mm_camera_socket_create(int cam_id, mm_camera_sock_type_t sock_type)
     memset(&sock_addr, 0, sizeof(sock_addr));
     sock_addr.addr_un.sun_family = AF_UNIX;
     snprintf(sock_addr.addr_un.sun_path,
-             UNIX_PATH_MAX, QCAMERA_DUMP_FRM_LOCATION"cam_socket%d", cam_id);
+             UNIX_PATH_MAX, "/data/vendor/qcam/cam_socket%d", cam_id);
     rc = connect(socket_fd, &sock_addr.addr, sizeof(sock_addr.addr_un));
     if (0 != rc) {
       close(socket_fd);